421c75efe88ac08ce0ee3d2787153864c1a9f22c
AnjheloMarreros <wmarrerosg1@upao.edu.pe>
2026-05-11 19:17:25 -0500

    Estructura básica de archivos a manera de esquema para funcionamiento básico

---
+++ backend/services/preprocess.py
@@ -0,0 +1,10 @@
+def procesar_texto(texto: str) -> str:
+    return texto.strip()
+
+def procesar_pdf(ruta_pdf: str) -> str:
+    # luego se implementa extracción
+    return ""
+
+def procesar_audio(ruta_audio: str) -> str:
+    # luego se implementa transcripción
+    return ""
